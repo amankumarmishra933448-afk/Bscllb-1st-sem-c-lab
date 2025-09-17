e30ee65159b020f95427215f9488b1983444b39b
amankumarmishra933448-afk <amankumarmishra933448@gmail.com>
2025-09-17 15:23:56 +0530

    Add files via upload

---
+++ number of n times.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int counter ,n;
+    printf("enter the number of times you want your name to be repeated:\t");
+    scanf("%d", &n);
+
+    for(counter=1; counter<=n; counter++);
+        printf("aman\n");
+
+}
