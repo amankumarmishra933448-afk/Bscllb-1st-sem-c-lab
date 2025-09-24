0fae6d5da0d41a38ef2b02efb840a22891c4b6cd
amankumarmishra933448-afk <amankumarmishra933448@gmail.com>
2025-09-24 15:29:30 +0530

    Add files via upload

---
+++ character is a vowel or not using.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("enter the alphabet from user")
+    scanf("%c", &ch);
+
+    switch(ch)
+    {
+    case 'a':
+        printf("vowel");
+        break;
+
+        case 'e':
+            printf("vowel");
+            break;
+
+        case 'i':
+            printf("vowel");
+            break;
+
+        case 'o':
+            printf("vowel");
+            break;
+
+        case 'u':
+            printf("vowel");
+            break;
+        default:
+            printf("consonant");
+
+    }return 0;
+}
